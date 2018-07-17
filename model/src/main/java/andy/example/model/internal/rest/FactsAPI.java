d8df382a96a8e89a9e2a2d7f90261cfa9d4d64c9
andy <anand.shinde85@gmail.com>
2018-07-17 22:14:38 +0530

    - Used constants for URL.

--- model/src/main/java/andy/example/model/internal/rest/FactsAPI.java
+++ model/src/main/java/andy/example/model/internal/rest/FactsAPI.java
@@ -1,18 +1,21 @@
 package andy.example.model.internal.rest;
 
+import andy.example.commons.utils.Constants;
 import andy.example.model.entities.FactsResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 /**
  * Retrofit interface to access facts API
+ *
  * @author Anand Shinde
  */
 public interface FactsAPI {
   /**
    * Method to retrieve facts from FactsAPI
-   * @return -
+   *
+   * @return - Facts response
    */
-  @GET("s/2iodh4vg0eortkl/facts.json")
+  @GET(Constants.URL_GET_FACTS)
   Call<FactsResponse> getFacts();
 }
