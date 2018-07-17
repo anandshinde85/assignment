b856adf8119d74eb48d92d4fea87ec68e4ffa572
andy <anand.shinde85@gmail.com>
2018-07-17 18:01:47 +0530

    - Added Constants.

---
+++ commons/src/main/java/andy/example/commons/utils/Constants.java
@@ -0,0 +1,14 @@
+package andy.example.commons.utils;
+
+/**
+ * Class holding Constants
+ *
+ * @author Anand Shinde
+ */
+public class Constants {
+  private Constants() {
+    // private constructor to avoid creating object of this class
+  }
+
+  public static final String BASE_URL = "https://dl.dropboxusercontent.com";
+}
