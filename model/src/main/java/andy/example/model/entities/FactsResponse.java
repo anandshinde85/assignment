50eabc1ce596c75472c9038ad123a514fc586e09
andy <anand.shinde85@gmail.com>
2018-07-17 21:08:05 +0530

    - Added entity class which encapsulates FactsAPI response.

---
+++ model/src/main/java/andy/example/model/entities/FactsResponse.java
@@ -0,0 +1,64 @@
+package andy.example.model.entities;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.List;
+
+/**
+ * Entity class encapsulating Facts API response
+ *
+ * @author Anand Shinde
+ */
+public class FactsResponse implements Parcelable {
+  private String title;
+  private List<Row> rows = null;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public List<Row> getRows() {
+    return rows;
+  }
+
+  public void setRows(List<Row> rows) {
+    this.rows = rows;
+  }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeString(this.title);
+    dest.writeTypedList(this.rows);
+  }
+
+  public FactsResponse() {
+  }
+
+  protected FactsResponse(Parcel in) {
+    this.title = in.readString();
+    this.rows = in.createTypedArrayList(Row.CREATOR);
+  }
+
+  public static final Parcelable.Creator<FactsResponse> CREATOR =
+      new Parcelable.Creator<FactsResponse>() {
+        @Override
+        public FactsResponse createFromParcel(Parcel source) {
+          return new FactsResponse(source);
+        }
+
+        @Override
+        public FactsResponse[] newArray(int size) {
+          return new FactsResponse[size];
+        }
+      };
+}
