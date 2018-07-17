2f5d5e8b832b81073d16e4cce53322c8d0caecf5
andy <anand.shinde85@gmail.com>
2018-07-17 23:10:35 +0530

    - Added service fetch facts.

---
+++ model/src/main/java/andy/example/model/service/FactsService.java
@@ -0,0 +1,18 @@
+package andy.example.model.service;
+
+import andy.example.model.callbacks.FactsServiceCallback;
+
+/**
+ * Service to fetch facts using retrofit API.
+ *
+ * @author Anand Shinde
+ */
+public interface FactsService {
+  /**
+   * Method to retrieve facts.
+   *
+   * @param factsServiceCallback - callback handler for passing appropriate response
+   * @param conversationId       - unique server interaction id
+   */
+  void getFacts(FactsServiceCallback factsServiceCallback, String conversationId);
+}
