140683c2abe3f64d1857388cb4a8f529ab21d6fa
andy <anand.shinde85@gmail.com>
2018-07-17 23:11:45 +0530

    - Added callback listener for passing network response to calling place.

---
+++ model/src/main/java/andy/example/model/callbacks/FactsServiceCallback.java
@@ -0,0 +1,26 @@
+package andy.example.model.callbacks;
+
+import andy.example.model.entities.FactsResponse;
+
+/**
+ * Callback handler for facts service which will notify presenter about success and failure of
+ * API call
+ *
+ * @author Anand Shinde
+ */
+public interface FactsServiceCallback {
+  /**
+   * Method to be called when response is success
+   *
+   * @param factsResponse  - instance holding facts
+   * @param conversationId - unique server interaction id
+   */
+  void onSuccess(FactsResponse factsResponse, String conversationId);
+
+  /**
+   * Method to notify there was failure while retrieving facts
+   *
+   * @param conversationId - unique server interaction id
+   */
+  void onFailure(String conversationId);
+}
