22f94d3bc980a03034f806c8fdb1bf19d53f30d8
andy <anand.shinde85@gmail.com>
2018-07-17 22:13:51 +0530

    - Added constant URL for get facts API.

--- commons/src/main/java/andy/example/commons/utils/Constants.java
+++ commons/src/main/java/andy/example/commons/utils/Constants.java
@@ -10,5 +10,9 @@ public class Constants {
     // private constructor to avoid creating object of this class
   }
 
+  // Base URL for all API
   public static final String BASE_URL = "https://dl.dropboxusercontent.com/";
+
+  // Get facts URL
+  public static final String URL_GET_FACTS = "s/2iodh4vg0eortkl/facts.json";
 }
