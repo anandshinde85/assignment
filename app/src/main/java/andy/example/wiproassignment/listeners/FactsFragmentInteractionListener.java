12d7688b75cba9bfcabd6a402fd298079e27e9a7
andy <anand.shinde85@gmail.com>
2018-07-18 21:45:08 +0530

    - Added fragment interaction listener.

---
+++ app/src/main/java/andy/example/wiproassignment/listeners/FactsFragmentInteractionListener.java
@@ -0,0 +1,15 @@
+package andy.example.wiproassignment.listeners;
+
+/**
+ * Interaction listener to handle/pass feedback from fragment to activity
+ *
+ * @author Anand Shinde
+ */
+public interface FactsFragmentInteractionListener {
+  /**
+   * Method to update toolbar title.
+   *
+   * @param title - updated title to be set
+   */
+  void updateToolbarTitle(String title);
+}
