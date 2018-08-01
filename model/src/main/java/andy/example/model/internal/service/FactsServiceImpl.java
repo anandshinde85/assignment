eac4fd043ef4a4d16a54ceead23a4ff98085ba3f
andy <anand.shinde85@gmail.com>
2018-08-01 20:22:21 +0530

    - Addressed review comment. (Empty row with empty data needs to be removed.)

--- model/src/main/java/andy/example/model/internal/service/FactsServiceImpl.java
+++ model/src/main/java/andy/example/model/internal/service/FactsServiceImpl.java
@@ -1,8 +1,10 @@
 package andy.example.model.internal.service;
 
+import android.text.TextUtils;
 import andy.example.commons.utils.RestAdapter;
 import andy.example.model.callbacks.FactsServiceCallback;
 import andy.example.model.entities.FactsResponse;
+import andy.example.model.entities.Row;
 import andy.example.model.internal.rest.FactsAPI;
 import andy.example.model.service.FactsService;
 import andy.example.model.utils.BaseError;
@@ -10,6 +12,8 @@ import andy.example.model.utils.BaseService;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -41,6 +45,15 @@ public class FactsServiceImpl extends BaseService<FactsResponse> implements Fact
     @Override
     protected void handleResponse(FactsResponse responseEntity,
                                   Response<FactsResponse> retrofitResponse, String conversationId) {
+        List<Row> rows = responseEntity.getRows();
+        Iterator<Row> rowIterator = rows.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            if (null == row || (TextUtils.isEmpty(row.getTitle()) && TextUtils.isEmpty(row.getDescription()) &&
+                    TextUtils.isEmpty(row.getImageHref()))) {
+                rowIterator.remove();
+            }
+        }
         factsServiceCallback.onSuccess(responseEntity, conversationId);
     }
 
